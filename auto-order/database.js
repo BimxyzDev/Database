f3023b11cdf48085e6cfc3c91d55bb063030b262
BimxyzDev <bimaputra436123@gmail.com>
2026-03-22 21:45:55 +0700

    Update database.js

--- auto-order/database.js
+++ auto-order/database.js
@@ -1,4 +1,24 @@
 module.exports = {
-  users: [],
-  transactions: []
+  users: [
+    {
+      username: "bimxyz",
+      password: "bimxyz123",
+      purchases: []
+    },
+    {
+      username: "johndoe",
+      password: "johndoe123",
+      purchases: []
+    },
+    {
+      username: "sigit",
+      password: "sigit321",
+      purchases: []
+    },
+    {
+      username: "admin",
+      password: "admin123",
+      purchases: []
+    }
+  ]
 };
